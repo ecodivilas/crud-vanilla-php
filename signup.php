313bcb2b9958bbd15761a7a89f2a77daa064f7eb
ecodivilas <eleazar.odivilas01@gmail.com>
2023-12-30 03:02:16 +0800

    feature: Add initial database connection for signup module

--- signup.php
+++ signup.php
@@ -7,6 +7,39 @@
     <title>Sign Up</title>
 </head>
 <body>
+    <?php
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "school_directory";
+
+        // Create connection
+        $conn = new mysqli($server, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection error: " . $conn->connect_error);
+        }
+        
+        if(isset($_POST['signup_button'])) {
+            // echo 'Working baboom!'; //NOSONAR
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $confirmPassword = $_POST['confirmPassword'];
+            // echo $name; //NOSONAR
+
+            $sql = "INSERT INTO account (email, username, pass)
+                    VALUES ('$email', '$name', '$password')";
+
+            if ($conn->query($sql) === true){
+                echo "New record created successfully.";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            $conn->close();
+        }
+    ?>
     <div class="container">
         <div class="modal">
             <form action="" method="post" class="form">
