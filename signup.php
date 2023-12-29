71ab9a73e99847b351f3e1a2794eeb29b590458b
ecodivilas <eleazar.odivilas01@gmail.com>
2023-12-30 02:18:21 +0800

    feature: Add structure and design of the signup form

---
+++ signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/signup.css">
+    <title>Sign Up</title>
+</head>
+<body>
+    <div class="container">
+        <div class="modal">
+            <form action="" method="post" class="form">
+                <input type="text" name="name" placeholder="name">
+                <input type="text" name="email" placeholder="email">
+                <input type="text" name="password" placeholder="password">
+                <input type="text" name="confirmPassword" placeholder="Confirm Password">
+                <input type="submit" value="Sign Up" name="signup_button">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
